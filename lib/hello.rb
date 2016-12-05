4109c5c4a29b187f4acd3d126951530c612d98af
jfontech <jfontech@uncc.edu>
2016-12-05 03:30:12 +0000

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,4 @@
-# Default is World
-# Author: Julian Fontecha(jfontech@uncc.edu)
+puts "What's your name"
+my_name = gets.strip
 
-
-name = ARGV.first || "World"
-
-puts "Hello, #{name}!"
+puts "Hello, #{my_name}!"
